79a25f665aef9d9da812798f119e8b219ff95745
raiciCgt26 <sofgirl2601@gmail.com>
2026-02-11 19:53:36 -0400

    Primer commit: Mi Coffee Website

---
+++ asset/js/scripts.js
@@ -0,0 +1,42 @@
+// swiper js
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  // grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// nav open close
+const body = document.querySelector("body"),
+  navMenu = body.querySelector(".menu-content"),
+  navOpenBtn = body.querySelector(".navOpen-btn"),
+  navCloseBtn = navMenu.querySelector(".navClose-btn");
+
+if (navMenu && navOpenBtn) {
+  navOpenBtn.addEventListener("click", () => {
+    navMenu.classList.add("open");
+    body.style.overflowY = "hidden";
+  });
+}
+
+if (navMenu && navCloseBtn) {
+  navCloseBtn.addEventListener("click", () => {
+    navMenu.classList.remove("open");
+    body.style.overflowY = "scroll";
+  });
+}
+
+// change header bg color
+
+// scroll up buttom
+
+// nav link indicador
+
+// scroll reveal animation
